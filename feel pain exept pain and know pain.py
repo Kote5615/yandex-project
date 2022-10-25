ffb3ce2d823801d1c53e45b0c3bc67ed8b09d9ee
Kote5615 <82301930+Kote5615@users.noreply.github.com>
2022-10-25 23:09:26 +0300

    Add files via upload

--- feel pain exept pain and know pain.py
+++ feel pain exept pain and know pain.py
@@ -1,6 +1,6 @@
 import sys
 import sqlite3
-from PyQt5 import uic  # Импортируем uic
+from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 alphabet = ['а', 'б', 'в', 'г', 'д', 'е', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р',
@@ -11,26 +11,29 @@ class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('start menu.ui', self)  # Загружаем дизайн
-        # self.pushButton_add.clicked.connect(self.run)
-        self.database()
+        self.pushButton_add.clicked.connect(self.add_item)
+        for i in self.buttonGroup.buttons():
+            i.clicked.connect(self.run)
 
-    def database(self):
-        letter = 'х'
+    def database(self, letter):
         con = sqlite3.connect("sorts_db.sqlite")
         # Создание курсора
         cur = con.cursor()
         # Выполнение запроса и получение всех результатов
         result = cur.execute(f"""SELECT * FROM sorts WHERE Sort like '{letter}%'""").fetchall()
-        # Вывод результатов на экран
+        result.sort()
         for elem in result:
             print(*elem)
-
-
-
         con.close()
 
-    # def run(self):
-    #     print("OK")
+    def run(self):
+        letter = self.sender().text()
+        print(letter)
+        self.database(letter)
+
+    def add_item(self):
+        # INSERT INTO sorts(Sort) VALUES('Балерина')
+        print('Ok')
 
 
 if __name__ == '__main__':
